010701778db7cbcd27a3302564c62d84e28d32fe
carotorres3112 <torresrodriguez3112@gmail.com>
2026-02-27 09:33:21 -0500

    feat: marcar y desmarcar tareas como completadas con contador

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
 const subtitle = document.getElementById('subtitle');
 const emptyMsg = document.getElementById('emptyMsg');
+const contador = document.getElementById('contador');
 
 function actualizarMensaje(){
     const tareas = taskList.querySelectorAll('li');
+    const completadas = taskList.querySelectorAll('.tarea-completada');
     if(tareas.length === 0){
         emptyMsg.style.display = 'block'; 
+        contador.style.display = 'none';
     } else{
         emptyMsg.style.display = 'none';
+        contador.style.display = 'block';
+        contador.textContent = completadas.length + ' of ' + tareas.length + ' completed';
     }
 }
 //agregamos un evento click al botón de agregar tarea cuando precione Add
@@ -24,6 +29,13 @@ addBtn.addEventListener('click', function() {
 
     const li = document.createElement('li');
     li.textContent = texto;
+
+    li.addEventListener('click', function(e){
+        if(e.target !== btnEliminar){
+            li.classList.toggle('tarea-completada');
+            actualizarMensaje();
+        }
+    });
 
     const btnEliminar = document.createElement('button');
     btnEliminar.textContent = 'x';
