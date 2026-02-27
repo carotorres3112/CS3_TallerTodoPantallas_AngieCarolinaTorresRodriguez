585a08b32f8afeb6dcd311c21e9ab4d6a0a546b8
carotorres3112 <torresrodriguez3112@gmail.com>
2026-02-27 09:10:45 -0500

    feat: eliminar tareas, mensaje No tasks yet y estilos del cuadro

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
 const subtitle = document.getElementById('subtitle');
+const emptyMsg = document.getElementById('emptyMsg');
 
+function actualizarMensaje(){
+    const tareas = taskList.querySelectorAll('li');
+    if(tareas.length === 0){
+        emptyMsg.style.display = 'block'; 
+    } else{
+        emptyMsg.style.display = 'none';
+    }
+}
 //agregamos un evento click al botón de agregar tarea cuando precione Add
 addBtn.addEventListener('click', function() {
     const texto = taskInput.value;
@@ -15,8 +24,20 @@ addBtn.addEventListener('click', function() {
 
     const li = document.createElement('li');
     li.textContent = texto;
+
+    const btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'x';
+    btnEliminar.classList.add('btn-eliminar');
+
+    btnEliminar.addEventListener('click', function() {
+        taskList.removeChild(li);
+        actualizarMensaje();
+    });
+
+    li.appendChild(btnEliminar);
     taskList.appendChild(li);
 
     taskInput.value = '';
-
+    actualizarMensaje();
 });
+actualizarMensaje();
